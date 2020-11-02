329cfc66d1e850cd933d8e36018c20d3274543a8
Mukesh Jaiswal <mukeshj92004@Mukeshs-MacBook-Pro.local>
2020-11-02 11:40:28 +0530

    ALLOWED_HOSTS added

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'postgres2',
         'PASSWORD': 'mukSam@908',
         'HOST': '127.0.0.1',
         'PORT': '5432',
